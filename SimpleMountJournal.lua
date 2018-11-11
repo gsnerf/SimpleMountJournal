b784649f26679eec5af6224eb6b009f0cddfe978
gsnerf <gsnerf@gsnerf.de>
2018-11-11 02:30:22 +0100

    Reworked window update mechanism to show group information on the button instead of mount specific ones where possible.

--- SimpleMountJournal.lua
+++ SimpleMountJournal.lua
@@ -30,29 +30,34 @@ MountJournal_UpdateMountList = function()
     end
     
     -- determine how many entries actually shown in list
-    local numDisplayedMounts = C_MountJournal.GetNumDisplayedMounts()
-    --local numDisplayedMounts = numDisplayedMountsBlizz - database.OverallGroupedMounts + database.OverallGroupCount
-    --DEFAULT_CHAT_FRAME:AddMessage(GetTime().." SMJ: Need to show "..numDisplayedMounts.." mounts rather than "..C_MountJournal.GetNumDisplayedMounts())
+    local numDisplayedMountsBlizz = C_MountJournal.GetNumDisplayedMounts()
+    local numDisplayedMounts = numDisplayedMountsBlizz - database.OverallGroupedMounts + database.OverallGroupCount
+    DEFAULT_CHAT_FRAME:AddMessage(GetTime().." SMJ: Need to show "..numDisplayedMounts.." mounts rather than "..numDisplayedMountsBlizz)
     
     -- update buttons based on stuff to show
     local scrollFrame = MountJournal.ListScrollFrame
     local offset = HybridScrollFrame_GetOffset(scrollFrame)
     local buttons = scrollFrame.buttons
+    local skipCount = 0
     for i=1, #buttons do
 		local button = buttons[i]
 		local displayIndex = i + offset
         if ( displayIndex <= numDisplayedMounts and showMounts ) then
-            updateButtonWithMount(displayIndex, button)
+            --updateButtonWithMount(displayIndex, button)
+            local skipped = SMJ_updateButtonWithMount(displayIndex + skipCount, button)
+            if skipped then
+                skipCount = skipCount + 1
+                i = i - 1
+            end
         else
-            resetButton(button)
+            SMJ_resetButton(button)
         end
     end
     
     local totalHeight = numDisplayedMounts * MOUNT_BUTTON_HEIGHT
 	HybridScrollFrame_Update(scrollFrame, totalHeight, scrollFrame:GetHeight())
-	
-	
 end
+MountJournal.ListScrollFrame.update = MountJournal_UpdateMountList
 
 function GetOwnedMountsCount()
     local numOwned = 0
@@ -66,10 +71,66 @@ function GetOwnedMountsCount()
     return numOwned
 end
 
-function updateButtonWithMount(index, button)
-    local creatureName, spellID, icon, active, isUsable, sourceType, isFavorite, isFactionSpecific, faction, isFiltered, isCollected, mountID = C_MountJournal.GetDisplayedMountInfo(index)
-    local needsFanfare = C_MountJournal.NeedsFanfare(mountID)
 
+--[[
+    @param index index of the shown mount as needed by C_MountJournal.GetDisplayedMountInfo()
+    @param button the button element to update with the new info
+    @return true if index was skipped because it is already shown, false in any other case
+]]
+function SMJ_updateButtonWithMount(index, button)
+    local creatureName, spellID, icon, active, isUsable, _, isFavorite, isFactionSpecific, faction, _, isCollected, mountID = C_MountJournal.GetDisplayedMountInfo(index)
+
+    local mountGroup = AddOnTable.Database.MountIdModelGroupMap[mountID]
+    if (mountGroup ~= nil) then
+        DEFAULT_CHAT_FRAME:AddMessage(GetTime().." SMJ: Showing "..creatureName.." which is part of "..mountGroup)
+        if (not SMJ_isGroupButtonVisible(mountGroup)) then
+            SMJ_createGroupButton(index, mountGroup, button)
+            return false
+        end
+        return true
+    else
+        local needsFanfare = C_MountJournal.NeedsFanfare(mountID)
+        SMJ_updateButton(index, button, creatureName, spellID, icon, active, isUsable, isFavorite, isFactionSpecific, faction, isCollected)
+        return false
+    end
+end
+
+function SMJ_isGroupButtonVisible(mountGroup)
+    -- TODO: actually some how check if this is the case
+    return false
+end
+
+function SMJ_createGroupButton(index, groupName, button)
+    local localizedGroupName = groupName -- TODO: actually add localization!
+    local group = AddOnTable.Database.MountGroups[groupName]
+
+    local icon = nil
+    local isActive = false
+    local isUsable = false
+    local isFavorite = false
+    local isFactionSpecific = false
+    local faction = nil
+    local isCollected = false
+    local needsFanfare = false
+    
+    for _, mountID in ipairs(group) do
+        local _, _, mIcon, mActive, mIsUsable, _, mIsFavorite, mIsFactionSpecific, mFaction, _, mIsCollected = C_MountJournal.GetMountInfoByID(mountID)
+        local mNeedsFanfare = C_MountJournal.NeedsFanfare(mountID)
+        icon = icon and icon or mIcon
+        isActive = isActive or mActive
+        isUsable = isUseable or mIsUsable
+        isFavorite = isFavorite or mIsFavorite
+        isFactionSpecific = isFactionSpecific or mIsFactionSpecific
+        faction = faction and faction or mFaction
+        isCollected = isCollected or mIsCollected
+        needsFanfare = needsFanfare or mNeedsFanfare
+    end
+    
+    --[[ TODO: selection based on spellID cannot work right now, we have to somehow check against the spellIDs of the whole group here ]]
+    SMJ_updateButton(index, button, localizedGroupName, 0, icon, isActive, isUsable, isFavorite, isFactionSpecific, faction, isCollected)
+end
+
+function SMJ_updateButton(index, button, creatureName, spellID, icon, active, isUsable, isFavorite, isFactionSpecific, faction, isCollected)
     button.name:SetText(creatureName)
     button.icon:SetTexture(needsFanfare and COLLECTIONS_FANFARE_ICON or icon)
     button.new:SetShown(needsFanfare)
@@ -138,7 +199,7 @@ function updateButtonWithMount(index, button)
     end
 end
 
-function resetButton(button)
+function SMJ_resetButton(button)
     button.name:SetText("");
     button.icon:SetTexture("Interface\\PetBattles\\MountJournalEmptyIcon")
     button.index = nil
