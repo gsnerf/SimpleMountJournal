44cf40177b7ac1b2771e2a29515a7ff628195465
gsnerf <gsnerf@gsnerf.de>
2018-12-11 23:41:58 +0100

    Extended database with a whole bunch of new groups.

--- Database.lua
+++ Database.lua
@@ -2,7 +2,60 @@ local AddOnName, AddOnTable = ...
 local _
 
 local StaticMountGroups = {
+    ClassMounts = {
+        860, -- mage
+        861, -- priest
+        864, -- monk
+        865, -- hunter color variation #1
+        866, -- death knight
+        867, -- warrior
+        868, -- demon hunter
+        870, -- hunter color variation #2
+        872, -- hunter color variation #3
+        884, -- rogue color variation #1
+        885, -- paladin color variation #1
+        888, -- shaman
+        889, -- rogue color variation #2
+        890, -- rogue color variation #1
+        891, -- rogue color variation #1
+        892, -- paladin color variation #2
+        893, -- paladin color variation #3
+        894, -- paladin color variation #4
+        898, -- warlock color variation #1
+        930, -- warlock color variation #2
+        931, -- warlock color variation #3
+    },
+    CloudSerpents = {
+        448, -- jade
+        464, -- azure
+        465, -- golden
+        471, -- onyx
+        472, -- crimson
+        478, -- astral
+    },
+    DragonTurtle = {
+        452, -- green
+        492, -- black
+        493, -- blue
+        494, -- brown
+        495, -- purple
+        496, -- red
+    },
+    GreatDragonTurtle = {
+        453, -- red
+        497, -- green
+        498, -- black
+        499, -- blue
+        500, -- brown
+        501, -- purple
+    },
     Gron = { 607, 655 },
+    HeavenlyCloudSerpents = {
+        473, -- onyx
+        474, -- crimson
+        475, -- golden
+        477, -- azure
+    },
     Mechanostrider = {
         39,  -- red
         40,  -- blue
@@ -16,7 +69,40 @@ local StaticMountGroups = {
         88, -- yellow
         89, -- white
         90  -- green
-    }
+    },
+    NetherwingDrakes = {
+        186, -- onyx
+        187, -- azure
+        188, -- cobalt
+        189, -- purple
+        190, -- veridian
+        191, -- violet
+    },
+    Panther = {
+        451, -- onyx
+        456, -- sapphire
+        457, -- jade
+        458, -- ruby
+        459, -- sunstone
+    },
+    ProtoDrakes = {
+        262, -- red
+        263, -- black
+        264, -- blue
+        265, -- time-lost
+        266, -- plagued
+        267, -- violet
+        278, -- green
+        306, -- ironbound
+        307, -- rusted
+    },
+    ThunderingCloudSerpents = {
+        466, -- jade
+        504, -- august
+        517, -- ruby
+        542, -- cobalt
+        561, -- onyx
+    },
 }
 
 local Prototype = {
--- Localization.lua
+++ Localization.lua
@@ -2,14 +2,32 @@ local AddOnName, AddOnTable = ...
 
 if ( GetLocale() == "deDE" ) then
     AddOnTable["Localization"] = {
+        ["ClassMounts"] = "Klassenreittiere",
+        ["CloudSerpents"] = "Wolkenschlangen",
+        ["DragonTurtle"] = "Drachenschildkröte",
+        ["FastMechanostrider"] = "Schneller Roboschreiter",
+        ["GreatDragonTurtle"] = "Große Drachenschildkröte",
         ["Gron"] = "Gron",
+        ["HeavenlyCloudSerpents"] = "Himmlische Wolkenschlangen",
         ["Mechanostrider"] = "Roboschreiter",
-        ["FastMechanostrider"] = "Schneller Roboschreiter"
+        ["NetherwingDrakes"] = "Drachen der Netherschwingen",
+        ["Panther"] = "Panther",
+        ["ProtoDrakes"] = "Protodrachen",
+        ["ThunderingCloudSerpents"] = "Donnernde Wolkenschlangen",
     }
 else
     AddOnTable["Localization"] = {
+        ["ClassMounts"] = "Class Mounts",
+        ["CloudSerpents"] = "Cloud Serpents",
+        ["DragonTurtle"] = "Dragon Turtle",
+        ["FastMechanostrider"] = "Fast Mechanostrider",
+        ["GreatDragonTurtle"] = "Great Dragon Turtle",
         ["Gron"] = "Gron",
-        ["Mechanostrider"] = "Mechanistrider",
-        ["FastMechanostrider"] = "Fast Mechanostrider"
+        ["HeavenlyCloudSerpents"] = "Heavenly Cloud Serpents",
+        ["Mechanostrider"] = "Mechanostrider",
+        ["NetherwingDrakes"] = "Netherwing Drakes",
+        ["Panther"] = "Panther",
+        ["ProtoDrakes"] = "Proto-Drakes",
+        ["ThunderingCloudSerpents"] = "Thundering Cloud Serpents",
     }
 end
